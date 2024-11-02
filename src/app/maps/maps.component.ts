39daf48c9c44563b3af0e84436a4e77632317b52
rivacortez <rivaldo062134@gmail.com>
2024-11-02 00:41:29 -0500

    feat: add markers

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -12,9 +12,7 @@ export class MapsComponent implements OnInit, AfterViewInit {
 
   @ViewChild('mapContainer') mapContainer!: ElementRef;
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   ngAfterViewInit() {
     this.initMap();
@@ -24,9 +22,34 @@ export class MapsComponent implements OnInit, AfterViewInit {
     this.map = L.map(this.mapContainer.nativeElement, { zoomControl: true }).setView([-9.5524878, -74.9387723], 6);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19
-
+      maxZoom: 19,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
+
+
+    const pickupLocation = L.marker([-9.5524878, -74.9387723], {
+      icon: L.icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34]
+      }),
+      draggable: true
+    }).addTo(this.map);
+    pickupLocation.bindPopup('Punto de recogida').openPopup();
+
+
+    const destinationLocation = L.marker([-8.109052, -79.021534], {
+      icon: L.icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34]
+      }),
+      draggable: true
+    }).addTo(this.map);
+    destinationLocation.bindPopup('Punto de destino');
+
 
     setTimeout(() => {
       this.map?.invalidateSize();
@@ -40,7 +63,6 @@ export class MapsComponent implements OnInit, AfterViewInit {
       this.map.invalidateSize();
     }
   }
-
 
   @HostListener('window:resize', ['$event'])
   onResize() {
